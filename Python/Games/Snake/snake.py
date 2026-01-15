bcd85bf9f13952cd845d31893c848a21d3a6db52
Esra <esrasoeylemez@gmail.com>
2026-01-15 08:56:33 +0100

    new adjustment was being made

---
+++ Python/Games/Snake/snake.py
@@ -0,0 +1,21 @@
+import turtle 
+import time 
+import random 
+
+
+delay = 0.1 
+
+# Score 
+score = 0
+high_score = 0
+
+wn = turtle.Screen()
+wn.title("Snake Game by Esra yayaaa")
+wn.bgcolor()
+wn.setup(width=600, height=600)
+wn.tracer(0)
+
+
+wn.listen()
+wn.onkeypress ("w") 
+wn.onkeypress ()
